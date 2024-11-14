01ba94e803da34e8b86c9c457178ae918e1c5bd4
karan brar <brarkaran4082@gmail.com>
2024-11-13 17:31:11 -0700

    signup  page

--- Frontend-Customer/src/Login.jsx
+++ Frontend-Customer/src/Login.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React from 'react';
 
 function Login() {
   return (
     <>
-    <div className='border-2 border-gray-500'>
-     <form action="/submit-login" method="POST">
-      <label for="username">Username:</label>
-      <input type="text" id="username" name="username" required></input>
-      <br></br>
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" required></input>
-
-      <button type="submit">Log In</button>
-    </form>    
-    </div>
-
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="w-96 p-6 border-2 border-gray-400 rounded-lg">
+          <form action="/submit-login" method="POST" className="space-y-4">
+            <div className='text-gray-700 text-2xl'>Login</div>
+            <div>
+              <label htmlFor="email" className="text-gray-500 block">Email:</label>
+              <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                required 
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="text-gray-500 block">Password:</label>
+              <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                required 
+                className="w-full p-2 border border-gray-300 rounded"
+              />
+            </div>
+            <button 
+              type="submit" 
+              className="w-full py-2 bg-red-600 text-white rounded"
+            >
+              Log In
+            </button>
+          </form>    
+          <div className='py-6'>New to UniMeals? SignUp</div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
